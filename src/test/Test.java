eb4e108f18ae0a5d92fe2466c3efe5ceff649f11
wangxuan <wangxuan@finogeeks.com>
2020-10-18 17:42:56 +0800

    threesum

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,16 +1,26 @@
 package test;
 
 
+import java.util.regex.Pattern;
+
 public class Test {
 
 
     public static void main(String[] args) {
-        String str1 = "abc";
-        String str2 = new String("abc");
-        String str3 = str2.intern();
-        System.out.println(str1 == str2);//false
-        System.out.println(str2 == str3);//false
-        System.out.println(str1 == str3);//true
+//        String str1 = "abc";
+//        String str2 = new String("abc");
+//        String str3 = str2.intern();
+//        System.out.println(str1 == str2);//false
+//        System.out.println(str2 == str3);//false
+//        System.out.println(str1 == str3);//true
+
+        String text="abbc";
+        String reg="ab{1,3}+bc";
+
+        boolean matches = Pattern.matches(reg, text);
+        System.out.println(matches);
+
+
 
     }
 
