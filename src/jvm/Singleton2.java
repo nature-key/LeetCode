353782b50cdfedb25440b9fd1c22eb86c72e9058
wangxuan <wangxuan@finogeeks.com>
2021-01-12 23:57:44 +0800

    singleton

---
+++ src/jvm/Singleton2.java
@@ -0,0 +1,21 @@
+package jvm;
+
+public class Singleton2 {
+
+    private  static  volatile Singleton2 singleton2 = null;
+
+    public Singleton2() {
+    }
+
+    private static Singleton2 getInstance() {
+        if (singleton2 != null) {
+            synchronized (Singleton2.class) {
+                if (singleton2 != null) {
+                    singleton2 = new Singleton2();
+                }
+            }
+        }
+        return singleton2;
+    }
+
+}
