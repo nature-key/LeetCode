0642bc9b38b8799b0e4a749fbc97abe5c5ef35c9
wangxuan <wangxuan@finogeeks.com>
2022-02-01 15:35:44 +0800

    add

---
+++ src/阶段一/三角形最小路径和.java
@@ -0,0 +1,46 @@
+package 阶段一;
+
+import 阶段热身.number202102.INFO;
+
+import java.util.*;
+
+public class 三角形最小路径和 {
+
+    /**
+     * 字底向上
+     * @param triangle
+     * @return
+     */
+    public static int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        int[] dp = new int[n];
+
+        for (int i = 0; i < triangle.get(n - 1).size(); i++) {
+            dp[i] = triangle.get(n - 1).get(i);
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                dp[j] = triangle.get(i).get(j) + Math.min(dp[j], dp[j + 1]);
+            }
+        }
+        return dp[0];
+    }
+
+
+    public static int minimumTotal1(List<List<Integer>> triangle) {
+        int[] dp = new int[triangle.size()];
+
+        for (int i = triangle.size() - 1; i >= 0; i--) {
+            for (int j = 0; j < triangle.get(i).size(); j++) {
+                dp[j]=triangle.get(i).get(j)+Math.min(dp[j],dp[j+1]);
+            }
+        }
+        return dp[0];
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
