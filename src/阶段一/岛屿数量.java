93e0af7bf3ba4dc0a1970c50466f70709ec8ecc3
wangxuan <wangxuan@finogeeks.com>
2022-03-01 21:20:23 +0800

    gird

---
+++ src/阶段一/岛屿数量.java
@@ -0,0 +1,31 @@
+package 阶段一;
+
+public class 岛屿数量 {
+
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    count++;
+                }
+            }
+        }
+
+    }
+
+    public void dfs(char[][] grid, int i, int j) {
+        if (i < 0 || i < 0 ||
+                i >= grid.length || j >= grid[0].length
+                || grid[i][j] == '0') {
+            return;
+        }
+        grid[i][j] = '0';
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j - 1);
+    }
+
+}
